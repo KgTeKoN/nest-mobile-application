266c9ea4d435dfbd43d3d388ab10e240e46e38e9
KG_TeKoN <hasherama8@gmail.com>
2023-03-09 14:32:45 +0200

    setup config service

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getServerStart(port): string {
+    return `Server has been started at ${port} port`;
   }
 }
