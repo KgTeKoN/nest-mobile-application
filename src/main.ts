1c33252bf9d550ceb841e4a8bf901cf87cda0650
KG_TeKoN <hasherama8@gmail.com>
2023-03-09 18:48:02 +0200

    added cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(app.get<ConfigService>(ConfigService).get('app.port'));
 }
 bootstrap();
