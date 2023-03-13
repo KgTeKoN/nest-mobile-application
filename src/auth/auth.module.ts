ba0914fdcb36dc1523321d5c489819a3a7cd0d2e
KG_TeKoN <hasherama8@gmail.com>
2023-03-13 12:34:38 +0200

    added jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AdminRepo } from '../api/admin/admin.repo';
 import { CryptoService } from '../crypto/crypto.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, AdminRepo, CryptoService],
+  providers: [AuthService, AdminRepo, CryptoService, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,24 +3,43 @@ import { SignInAuthDto } from './dto/signIn-auth.dto';
 import { AdminRepo } from '../api/admin/admin.repo';
 import { CryptoService } from '../crypto/crypto.service';
 import * as argon from 'argon2';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private adminRepo: AdminRepo,
     private cryptoService: CryptoService,
+    private configService: ConfigService,
+    private jwtService: JwtService,
   ) {}
+
   async signIn(signInAuthDto: SignInAuthDto) {
     const admin = await this.adminRepo.findOne({ email: signInAuthDto.email });
     if (!admin) {
       throw new ForbiddenException('Credentials incorrect');
     }
+    const role = admin.isSuper ? 'superAdmin' : 'admin';
     const deCryptPw = await this.cryptoService.deCrypto(admin.password);
     const pwMatches = await argon.verify(deCryptPw, signInAuthDto.password);
     if (!pwMatches) {
       throw new ForbiddenException('Credentials incorrect');
     }
     delete admin.password;
-    return admin;
+    const secret = this.configService.get<string>('JWT_SECRET_KEY');
+
+    const accessToken = await this.jwtService.signAsync(
+      { ...admin, role },
+      {
+        expiresIn: '1h',
+        secret,
+      },
+    );
+
+    return {
+      success: true,
+      accessToken,
+    };
   }
 }
